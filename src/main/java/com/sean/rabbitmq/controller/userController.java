12f70baf6cea7aa4e0f40ad3cf0bc79c63fbb5c1
xuxin2009 <724056979@qq.com>
2020-05-14 17:58:01 +0800

    add a userController

---
+++ src/main/java/com/sean/rabbitmq/controller/userController.java
@@ -0,0 +1,18 @@
+package com.sean.rabbitmq.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : sean
+ * @version V1.0
+ * @Project: rabbitmq
+ * @Package com.sean.rabbitmq.controller
+ * @Description: TODO
+ * @date Date : 2020年05月14日 17:57
+ */
+
+@RestController
+public class userController {
+
+
+}
